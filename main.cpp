497e68c759f0267f5491a59901691b53c95837cb
ranimr <bettaieb.ranim@esprit.tn>
2023-11-16 15:23:56 +0100

    third commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-
+#include <QMessageBox>
+#include"connexion.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QTimer>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    machine m;
+     //m.enregistrerHistoriqueReparationManuelle();
+   Connexion c;
+    bool test=c.createconnect();
     MainWindow w;
+    if (test)
+    {
     w.show();
+    QMessageBox::information(nullptr,QObject::tr("database is open"),QObject::tr("connexion seccessuful.\n""click cancel to exit."),QMessageBox::Cancel);
+    }
+    else
+        QMessageBox::critical(nullptr,QObject::tr("database is not open"),QObject::tr("connexion failed.\n""click cancel to exit."),QMessageBox::Cancel);
     return a.exec();
 }
+
