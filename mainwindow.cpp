110b8d1371f31321f97663e97009be373469c212
ranimr <bettaieb.ranim@esprit.tn>
2023-11-16 15:22:52 +0100

    third commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,36 +1,56 @@
-include "mainwindow.h"
+#include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "machine.h"
+#include "map.h"
+#include "maintenance.h"
 #include<QDebug>
 #include<QApplication>
 #include<QMessageBox>
 #include <QIntValidator>
+#include<QtCharts>
+#include<QtSql>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->position->setItemData(0, QVariant(), Qt::UserRole - 1);
+    ui->position->addItem("");
+    ui->position->addItem("bloc1");
+    ui->position->addItem("bloc2");
+    ui->position->addItem("bloc3");
+    ui->position->addItem("bloc4");
 
+    ui->etat->setItemData(0, QVariant(), Qt::UserRole - 1);
+    ui->etat->addItem("");
+    ui->etat->addItem("fonctionelle");
+    ui->etat->addItem("en panne");
+
+    ui->fonction->setItemData(0, QVariant(), Qt::UserRole - 1);
+    ui->fonction->addItem("");
+    ui->fonction->addItem("labourage");
+    ui->fonction->addItem("récolte");
+     ui->fonction->addItem("fèrtilisation");
+      ui->fonction->addItem("semis");
+      ui->fonction->addItem("pulvérisation");
+
+      fenetreMaintenance = nullptr;
+    fenetremap=nullptr;
+    currentmachine=nullptr;
+
+    connect(ui->map, &QPushButton::clicked, this, &MainWindow::on_map_clicked);
     ui->tabm->setModel(m.afficher());
+    showMachineStatistics();
+
+
+
      ui->q_carburant->setValidator( new QIntValidator(0,200, this));
      ui->nb_heures->setValidator(new QIntValidator(0,1000, this));
-    QRegExp regex("[A-Za-z0-9]+");
+     QRegExp regex("\\d{3}tunisie\\d{4}");
      QRegExpValidator *validator = new QRegExpValidator(regex, this);
      ui->serie->setValidator(validator);
 
-     QRegExp reg("^(en panne|fonctionnelle)$");
-     QRegExpValidator *valid = new QRegExpValidator(reg, this);
-     ui->etat->setValidator(valid);
-     //Tracteur,Moissonneuse,�pandeur d'engrais,Pulv�risateur,Semoir
-
-    QRegExp re("^(labourage|recolte|fertilisation|semis|pulverisation)$");
-     QRegExpValidator *vali = new QRegExpValidator(re, this);
-     ui->fonction->setValidator(vali);
-
-
-     ui->chercher_2->setIcon(QIcon("///C:/Users/HP/Desktop/gestiondesmachines/chercher.png"));
-
-
+updateStatistics();
 }
 
 MainWindow::~MainWindow()
@@ -43,9 +63,9 @@ void MainWindow::on_ajouter_clicked()
 {
 
     QString serie=ui->serie->text();
-    QString etat=ui->etat->text();
-    QString fonction=ui->fonction->text();
-    QString position=ui->position->text();
+    QString etat=ui->etat->currentText();
+    QString fonction=ui->fonction->currentText();
+    QString position=ui->position->currentText();
      int nb_heures=ui->nb_heures->text().toInt();
       int q_carburant=ui->q_carburant->text().toInt();
       machine m (serie,etat,fonction,position,nb_heures,q_carburant);
@@ -114,28 +134,15 @@ void MainWindow::on_agri_clicked()
 
 
 
-void MainWindow::on_supprimer_clicked()
-{
 
-    QString serie=(ui->supp->text());
-    bool test=m1.supprimer(serie);
-    if (test)
-    {
-     QMessageBox::information(nullptr,QObject::tr("ok"),QObject::tr("suppression effectuer.\n""click cancel to exit."),QMessageBox::Cancel);
-     ui->tabm->setModel(m.afficher());
-    }
-    else
-        QMessageBox::critical(nullptr,QObject::tr("not ok"),QObject::tr("suppression non effectuer.\n""click cancel to exit."),QMessageBox::Cancel);
-
-}
 
 void MainWindow::on_modifier_clicked()
 {
     machine m;
     m.setserie(ui->serie->text());
-       m.setetat (ui->etat->text());
-        m.setfonction( ui->fonction->text());
-        m.setposition ( ui->position->text());
+       m.setetat (ui->etat->currentText());
+        m.setfonction( ui->fonction->currentText());
+        m.setposition ( ui->position->currentText());
         m.setq_carburant( ui->q_carburant->text().toInt());
        m.setnb_heures( ui->nb_heures->text().toInt());
 
@@ -149,23 +156,170 @@ void MainWindow::on_modifier_clicked()
         else
         {
 
-                QMessageBox::critical(this, tr("�chec de l'update"), tr("La mise � jour des donn�es a �chou�."));
+                QMessageBox::critical(this, tr("Échec de l'update"), tr("La mise à jour des données a échoué."));
         }
 }
 
-void MainWindow::on_chercher_2_clicked()
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString nomFichierPdf = QFileDialog::getSaveFileName(this, "Enregistrer en PDF", "", "Fichiers PDF (*.pdf)");
+
+        if (!nomFichierPdf.isEmpty()) {
+            machine m; // Créez une instance de la classe machine
+            bool exportReussi = m.exporterMachinesEnPannePdf(nomFichierPdf);
+
+            if (exportReussi) {
+                QMessageBox::information(this, "Exportation PDF réussie", "Les données des machines en panne ont été exportées en PDF.");
+            } else {
+                QMessageBox::critical(this, "Échec de l'exportation PDF", "L'exportation des données en PDF a échoué.");
+            }
+        }
+}
+void MainWindow::showMachineStatistics()
+{
+    QString sqlQuery = "SELECT serie, SUM(nombre_heure) AS heures_totales FROM machines GROUP BY serie";
+    QSqlQuery query;
+    query.prepare(sqlQuery);
+
+    QBarSeries *series = new QBarSeries();
+    QBarCategoryAxis *axisX = new QBarCategoryAxis();
+    if (query.exec()) {
+        while (query.next()) {
+            QString serie = query.value(0).toString();
+            double heuresTotales = query.value(1).toDouble();
+            QBarSet *barSet = new QBarSet(serie);
+            *barSet << heuresTotales;
+            series->append(barSet);
+            axisX->append(serie);
+        }
+    } else {
+        qDebug() << "Erreur d'exécution de la requête : " << query.lastError().text();
+    }
+
+    // Création du graphique
+    QChart *chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Statistiques des machines par serie");
+    chart->setAnimationOptions(QChart::SeriesAnimations);
+
+    QValueAxis *axisY = new QValueAxis();
+    chart->setAxisY(axisY, series);
+
+    QChartView *chartView = new QChartView(chart);
+
+    // Ajoutez le QChartView au widget dans votre interface utilisateur (chartContainer)
+    QWidget *chartContainer = ui->chartContainer; // Assurez-vous que le nom correspond à celui dans votre fichier .ui
+    QVBoxLayout *layout = new QVBoxLayout(chartContainer);
+    layout->addWidget(chartView);
+    connect(ui->tabm->model(), &QAbstractItemModel::dataChanged, this, &MainWindow::updateStatistics);
+}
+
+void MainWindow::updateStatistics()
+{
+    QLayout *containerLayout = ui->chartContainer->layout();
+    if (containerLayout) {
+        // Attempt to find the chart or chart view in the layout
+        QChart *chart = nullptr;
+
+        // Check if the widget at index 0 is a QChartView
+        if (containerLayout->count() > 0) {
+            QWidget *widgetAtIndex0 = containerLayout->itemAt(0)->widget();
+            QChartView *chartView = dynamic_cast<QChartView*>(widgetAtIndex0);
+
+            if (chartView) {
+                chart = chartView->chart();
+            }
+        }
+
+        // If not found, check if the widget itself is a QChart
+        if (!chart) {
+            QChart *chartWidget = dynamic_cast<QChart*>(ui->chartContainer);
+            if (chartWidget) {
+                chart = chartWidget;
+            }
+        }
+
+        if (chart) {
+            QBarSeries *series = dynamic_cast<QBarSeries*>(chart->series().at(0));
+
+            if (series) {
+                // Rest of your update logic here
+                QString sqlQuery = "SELECT serie, SUM(nombre_heure) AS heures_totales FROM machines GROUP BY serie";
+                QSqlQuery query;
+                query.prepare(sqlQuery);
+
+                // Rest of your update logic here...
+
+                qDebug() << "Chart updated successfully.";
+                return;
+            } else {
+                qDebug() << "Series not found in the chart.";
+            }
+        } else {
+            qDebug() << "Chart not found in the layout.";
+        }
+    } else {
+        qDebug() << "No layout in the chartContainer.";
+    }
+}
+
+
+void MainWindow::on_rechercher_clicked()
 {
     machine m;
-    m.setserie(ui->serie->text());
+    m.setserie(ui->chercher->text());
 
+    QSqlQueryModel *result = m.rechercherParSerie(m.getserie());
 
-   QSqlQueryModel *result = m.rechercherParSerie(m.getserie());
-
-    if (result) {
+    if (result && result->rowCount() > 0) {
         ui->tabm->setModel(result);
-        QMessageBox::information(nullptr,QObject::tr("ok"),QObject::tr("recherche effectuer.\n""click cancel to exit."),QMessageBox::Cancel);
-
+        QMessageBox::information(nullptr, QObject::tr("ok"), QObject::tr("Recherche effectuée.\nCliquez sur Annuler pour quitter."), QMessageBox::Cancel);
     } else {
-        QMessageBox::critical(this, tr("�chec de la recherche"), tr("La recherche a �chou�."));
+        QMessageBox::critical(this, tr("Échec de la recherche"), tr("La recherche a échoué ou la série n'existe pas."));
     }
+}
+
+void MainWindow::on_supp_clicked()
+{ QString serie=(ui->chercher->text());
+    bool test=m1.supprimer(serie);
+    if (test)
+    {
+     QMessageBox::information(nullptr,QObject::tr("ok"),QObject::tr("suppression effectuer.\n""click cancel to exit."),QMessageBox::Cancel);
+     ui->tabm->setModel(m.afficher());
+    }
+    else
+        QMessageBox::critical(nullptr,QObject::tr("not ok"),QObject::tr("suppression non effectuer.\n""click cancel to exit."),QMessageBox::Cancel);
+
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if (fenetreMaintenance)
+    {
+        // Assurez-vous que la fenêtre maintenance existe
+        fenetreMaintenance->enregistrerEtAfficher();
+        fenetreMaintenance->show();
+
+    }
+    else
+    {
+        // Si la fenêtre maintenance n'existe pas, créez-la et affichez-la
+        fenetreMaintenance = new maintenance(this);
+        fenetreMaintenance->show();
+    }
+}
+
+void MainWindow::on_map_clicked()
+{
+
+    if (!fenetremap) {
+            fenetremap = new map(this);
+        }
+        fenetremap->show();
+        QString machinePosition = ui->position->currentText();
+            QString machineSerie = ui->serie->text();
+            fenetremap->updateLabelFromMachine(machinePosition, machineSerie);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include<QObject>
 #include <QMainWindow>
 #include"machine.h"
+#include"maintenance.h"
+#include"map.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -11,10 +13,13 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+
 public:
     MainWindow(QWidget *parent = nullptr);
+    static void qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a);
     ~MainWindow();
-private slots:
+
+public slots:
 
     void on_ajouter_clicked();
 
@@ -28,15 +33,33 @@ private slots:
 
     void on_agri_clicked();
 
-    void on_supprimer_clicked();
+
 
     void on_modifier_clicked();
 
-    void on_chercher_2_clicked();
+
+    void on_pushButton_3_clicked();
+    void showMachineStatistics();
+    void updateStatistics();
+    void on_rechercher_clicked();
+
+    void on_supp_clicked();
+
+   // void on_alerte_clicked();
+
+
+private slots:
+    void on_pushButton_clicked();
+
+
+    void on_map_clicked();
 
 private:
     Ui::MainWindow *ui;
     machine m;
     machine m1;
+     maintenance *fenetreMaintenance;
+     map *fenetremap;
+     machine *currentmachine;
 };
 #endif // MAINWINDOW_H
